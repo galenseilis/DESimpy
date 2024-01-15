13edaaeb0d0d0e82c583da3422353443e9afca69
galenseilis <galen.seilis@seilis.ca>
2024-01-14 20:50:16 -0800

    Added a couple of distributions

--- src/desimpy/distributions.py
+++ src/desimpy/distributions.py
@@ -1,10 +1,38 @@
 import abc
 import random
-
+from typing import NoReturn
 
 class Distribution(abc.ABC):
 	
 	@abc.abstractmethod
-	def sample(self, env):
+	def sample(self):
 		'''Return a sampled thing.'''
 		raise NotImplementedError
+
+
+class ContinuousUniform(Distribution):
+	'''Continuous uniform distribution between a lower and upper value.'''
+
+	def __init__(self, lower: float, upper:float) -> NoReturn:
+		'''Initialize distribution with lower/upper bounds as parameters.
+
+		Args:
+			lower (float): Lower bound of distribution.
+			upper (float): Upper bound of distribution.
+		'''
+		self.lower = lower
+		self.upper = upper
+
+	def sample(self):
+		'''Sample from the distribution.'''
+		return random.uniform(self.lower, self.upper)
+
+class Triangular(Distribution):
+	
+	def __init__(self, lower: float, high: float, mode: float) -> NoReturn:
+		self.lower = lower
+		self.higher = higher
+		self.mode = mode
+	
+	def sample(self):
+		return random.triangular(self.lower, self.higher, self.mode)
