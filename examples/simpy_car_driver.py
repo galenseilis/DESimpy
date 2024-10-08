e28e38131c3c608c7f324071a119bd2d26de40c5
galenseilis <galen.seilis@seilis.ca>
2024-10-07 21:50:41 -0700

    example: driver process interrupting car process

--- examples/simpy_car_driver.py
+++ examples/simpy_car_driver.py
@@ -1,5 +1,26 @@
-# TODO: Fix, and use built-in event deactivation rather than bool flag.
-from desimpy.des import EventScheduler
+"""
+```yaml
+description: Simulate a driver process interrupting a car charging process.
+source: https://simpy.readthedocs.io/en/latest/simpy_intro/process_interaction.html#interrupting-another-process
+contents:
+    - 0. Imports.
+    - 1. Define Car Class.
+    - 2. Define Driver Process.
+    - 3. Initialize Event Scheduler.
+    - 4. Register Processes.
+    - 5. Run Simulation.
+```
+"""
+
+##############
+# $0 IMPORTS #
+##############
+
+from desimpy.des import Event, EventScheduler
+
+#######################
+# $1 DEFINE CAR CLASS #
+#######################
 
 
 class Car:
@@ -10,40 +31,51 @@ class Car:
 
     def schedule_run(self) -> None:
         """Schedule the initial run event."""
-        self.env.timeout(0, self.run)
+        self.env.timeout(0, self.schedule_charge)
 
-    def run(self) -> None:
+    def schedule_drive(self):
+        # Define the action to be executed when charging ends
+        print(f"Start driving at {self.env.current_time}")
+
+        # Schedule the next parking and charging event
+        self.env.timeout(2, self.schedule_charge)
+
+    def schedule_charge(self) -> None:
         """Handle the parking and charging, followed by driving."""
         print(f"Start parking and charging at {self.env.current_time}")
 
-        # Define the action to be executed when charging ends
-        def charge_action() -> None:
-            print(f"Start driving at {self.env.current_time}")
-
-            # Schedule the next parking and charging event
-            self.env.timeout(2, self.run)
-
         # Schedule the charge process
-        self.env.timeout(5, charge_action)
+        self.env.timeout(5, self.schedule_drive)
 
 
-def driver(env: EventScheduler) -> None:
-    """Driver process that interrupts the car."""
+############################
+# $2 DEFINE DRIVER PROCESS #
+############################
 
-    def interrupt(env: EventScheduler):
-        env.deactivate_next_event()
+
+def driver(env, car):
+    def interrupt_action():
+        print("Was interrupted. Hope, the battery is full enough ...")
+        env.interrupt_next_event(next_event=Event(env.current_time, car.schedule_drive))
 
     env.timeout(3, interrupt_action)
 
 
-# Initialize the event scheduler
+#################################
+# $3 INITIALIZE EVENT SCHEDULER #
+#################################
+
 scheduler = EventScheduler()
 
-# Create a car instance
+#########################
+# $4 REGISTER PROCESSES #
+#########################
+
 car = Car(scheduler)
+driver(scheduler, car)
 
-# Schedule the driver process
-scheduler.timeout(0, lambda: driver(scheduler))
+#####################
+# $5 RUN SIMULATION #
+#####################
 
-# Run the simulation
 scheduler.run_until_max_time(15)
