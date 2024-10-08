dcaf02efa4f40929809204339204a3f0e262c50b
galenseilis <galen.seilis@seilis.ca>
2024-10-07 19:34:13 -0700

    example: interruption mechnaism

---
+++ examples/interrupt.py
@@ -0,0 +1,76 @@
+"""
+```yaml
+contents:
+    - 0. Imports
+```
+"""
+
+##############
+# $0 IMPORTS #
+##############
+from desimpy.des import Event, EventScheduler
+
+################################
+# $1 INTERRUPT BY DEACTIVATION #
+################################
+
+print("INTERRUPT BY DEACTIVATION")
+env = EventScheduler()
+
+env.timeout(5, lambda: "foo", context={"meow": 0})
+env.timeout(0, lambda: env.interrupt_next_event(), context={"hi": "bye"})
+env.timeout(10, lambda: "bar", context={"woof": 1})
+
+results = env.run_until_max_time(6)
+
+for result in results:
+    event, outcome = result
+    print(event.time, outcome, event.context, event.active)
+
+################################
+# $2 INTERRUPT BY CANCELLATION #
+################################
+
+print("\nINTERRUPT BY CANCELLATION")
+
+env = EventScheduler()
+
+env.timeout(5, lambda: "foo", context={"meow": 0})
+
+
+def interrupt_action():
+    env.interrupt_next_event(method="cancel")
+    return "destroyer of worlds"
+
+
+env.timeout(0, interrupt_action, context={"hi": "bye"})
+env.timeout(10, lambda: "bar", context={"woof": 1})
+
+results = env.run_until_max_time(6)
+
+for result in results:
+    event, outcome = result
+    print(event.time, outcome, event.context, event.active)
+
+#################################################
+# $3 INTERRUPT BY DEACTIVATION AND CUSTOM EVENT #
+#################################################
+
+print("\nINTERRUPT BY DEACTIVATION WITH NEW EVENT")
+env = EventScheduler()
+
+env.timeout(5, lambda: "foo", context={"meow": 0})
+env.timeout(
+    0,
+    lambda: env.interrupt_next_event(
+        next_event=Event(30, lambda: "hehe, made it!", context={"the": "end"})
+    ),
+    context={"hi": "bye"},
+)
+env.timeout(10, lambda: "bar", context={"woof": 1})
+
+results = env.run_until_max_time(100)
+
+for result in results:
+    event, outcome = result
+    print(event.time, outcome, event.context, event.active)
