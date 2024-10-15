403b2047d6823c597ffab21fd1d0e8b20e311fed
galenseilis <galen.seilis@seilis.ca>
2024-10-14 19:40:27 -0700

    fix: interrupts to match current imp; set machine number back to 10

--- examples/interrupt.py
+++ examples/interrupt.py
@@ -4,7 +4,6 @@ contents:
     - 0. Imports
     - 1. Interrupt by Deactivation
     - 2. Interrupt by Cancellation
-    - 3. Interrupt by Deactivation and Custom Event
 ```
 """
 
@@ -22,7 +21,7 @@ print("INTERRUPT BY DEACTIVATION")
 env = EventScheduler()
 
 env.timeout(5, lambda: "foo", context={"meow": 0})
-env.timeout(0, lambda: env.interrupt_next_event(), context={"hi": "bye"})
+env.timeout(0, lambda: env.deactivate_next_event(), context={"hi": "bye"})
 env.timeout(10, lambda: "bar", context={"woof": 1})
 
 results = env.run_until_max_time(6)
@@ -43,37 +42,14 @@ env.timeout(5, lambda: "foo", context={"meow": 0})
 
 
 def interrupt_action():
-    env.interrupt_next_event(method="cancel")
+    env.cancel_next_event()
     return "destroyer of worlds"
 
 
 env.timeout(0, interrupt_action, context={"hi": "bye"})
 env.timeout(10, lambda: "bar", context={"woof": 1})
 
-results = env.run_until_max_time(6)
-
-for result in results:
-    event, outcome = result
-    print(event.time, outcome, event.context, event.active)
-
-#################################################
-# $3 INTERRUPT BY DEACTIVATION AND CUSTOM EVENT #
-#################################################
-
-print("\nINTERRUPT BY DEACTIVATION WITH NEW EVENT")
-env = EventScheduler()
-
-env.timeout(5, lambda: "foo", context={"meow": 0})
-env.timeout(
-    0,
-    lambda: env.interrupt_next_event(
-        next_event=Event(30, lambda: "hehe, made it!", context={"the": "end"})
-    ),
-    context={"hi": "bye"},
-)
-env.timeout(10, lambda: "bar", context={"woof": 1})
-
-results = env.run_until_max_time(100)
+results = env.run_until_max_time(11)
 
 for result in results:
     event, outcome = result
