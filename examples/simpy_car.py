89be0c1325f5c2e544ba25c334381e0cce4b3199
galenseilis <galen.seilis@seilis.ca>
2024-10-14 19:49:41 -0700

    doc: add docstrings to simpy car example

--- examples/simpy_car.py
+++ examples/simpy_car.py
@@ -1,4 +1,9 @@
 """
+This script demonstrates a simple car parking and driving process simulation using a custom discrete-event 
+simulation (DES) framework. It closely follows the basic SimPy example where a car alternates between parking 
+and driving. The script schedules and executes events, showing how the car switches states after specific 
+time intervals.
+
 ```yaml
 contents:
     - 0. Imports
@@ -22,10 +27,25 @@ from desimpy.des import Event, EventScheduler
 
 
 def car(env: EventScheduler) -> None:
-    """The car process."""
+    """
+    Simulates the car process that alternates between parking and driving.
+
+    The car parks for 5 time units and then drives for 2 time units, after 
+    which it returns to parking. This process repeats until the simulation ends.
+
+    Args:
+        env (EventScheduler): The simulation environment responsible for 
+            scheduling and managing events.
+
+    """
     print(f"Start parking at {env.current_time}")
 
     def end_parking_action() -> None:
+        """
+        Action to transition the car from parking to driving. After driving for 2 
+        time units, the car will park again.
+
+        """
         print(f"Start driving at {env.current_time}")
         env.timeout(2, action=lambda: car(env))
 
