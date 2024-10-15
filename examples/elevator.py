b4c46d2e05d1277af25903c079fb2e27d8f4457b
galenseilis <galen.seilis@seilis.ca>
2024-10-14 20:57:05 -0700

    example: add draft of elevator as reminder for later

---
+++ examples/elevator.py
@@ -0,0 +1,7 @@
+"""
+```yaml
+source: https://stats.stackexchange.com/questions/447118/queuing-theory-for-elevators
+```
+"""
+
+raise NotImplementedError("This example has not been implemented yet.")
