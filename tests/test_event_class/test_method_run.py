ac1da555b3ff394d397be79c20810ee1091f6a9e
galenseilis <galen.seilis@seilis.ca>
2024-10-20 16:57:06 -0700

    test: add event schedule example

--- tests/test_event_class/test_method_run.py
+++ tests/test_event_class/test_method_run.py
@@ -1,5 +1,6 @@
+import heapq
 import pytest
-from desimpy import Event
+from desimpy import Event, EventScheduler
 
 def test_no_action():
     event = Event(2018)
@@ -11,3 +12,31 @@ def test_lambda_none():
     result = event.run()
     assert result is None
 
+def test_action_returns_literal():
+    value = 'foo'
+    event = Event(2018, action=lambda: value)
+    result = event.run()
+    assert result == value
+
+def test_event_that_times_out_event():
+    env = EventScheduler()
+
+    time1 = 2
+    action1 = lambda: env.timeout(30)
+    event1 = Event(time1, action1)
+
+    env.schedule(event1)
+    event1.run()
+    
+    assert len(env.event_queue) == 2
+    assert env.next_event() == event1
+    step1 = env.step()
+    assert step1[0].time == 2
+    step2 = env.step()
+    assert step2[0].time == 30
+    assert step2[1] == None
+
+    # WARN: Not sure if this last event should exist.
+    step3 = env.step()
+    assert step3[0].time == 32
+    assert step3[1] == None
