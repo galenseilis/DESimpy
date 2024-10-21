9cdabd66ac6e18f9be6209d24506caec4f9956b0
galenseilis <galen.seilis@seilis.ca>
2024-10-20 22:15:47 -0700

    test: scheduler timeout

---
+++ tests/test_eventscheduler_class/test_timeout.py
@@ -0,0 +1,13 @@
+from desimpy import Event, EventScheduler
+
+def test_event_with_context():
+    delay = 10
+    action = lambda: 2018
+    context = {'foo':'bar', 1:'baz'}
+    env = EventScheduler()
+    env.timeout(delay=delay, action=action, context=context)
+    (event_out, event_result) = env.step()
+    assert event_out.time == 10
+    assert event_out.action() == 2018
+    assert event_out.context == context
+    assert event_result == 2018
