a39642e66fe383efcca210f5b8562a718fbc7582
galenseilis <galen.seilis@seilis.ca>
2024-11-23 16:53:15 -0800

    delete elevator example until I actually implement it.

--- examples/elevator.py
+++
@@ -1,6 +0,0 @@
-"""```yaml
-source: https://stats.stackexchange.com/questions/447118/queuing-theory-for-elevators
-```
-"""
-
-raise NotImplementedError("This example has not been implemented yet.")
