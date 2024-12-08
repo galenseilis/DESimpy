c6ee366fd33b6e0d93e7d3809025ea06e5be6bae
galenseilis <galen.seilis@seilis.ca>
2024-12-08 06:53:01 -0800

    fix:tests for peek_by_condition

--- tests/test_eventscheduler_class/test_peek_by_condition.py
+++ tests/test_eventscheduler_class/test_peek_by_condition.py
@@ -14,24 +14,24 @@ def test_next_by_time():
 
     # NOTE: Cases related to t=-1
     assert env.peek_by_condition(lambda env, event: event.time < -1) is None
-    assert env.next_event_by_condition(lambda env, event: event.time <= -1) == event0
-    assert env.next_event_by_condition(lambda env, event: event.time == -1) == event0
-    assert env.next_event_by_condition(lambda env, event: event.time >= -1) == event0
-    assert env.next_event_by_condition(lambda env, event: event.time > -1) is event1
+    assert env.peek_by_condition(lambda env, event: event.time <= -1) == -1
+    assert env.peek_by_condition(lambda env, event: event.time == -1) == -1
+    assert env.peek_by_condition(lambda env, event: event.time >= -1) == -1
+    assert env.peek_by_condition(lambda env, event: event.time > -1) is 0
 
     # NOTE: Cases related to t=0
-    assert env.next_event_by_condition(lambda env, event: event.time < 0) is event0
-    assert env.next_event_by_condition(lambda env, event: event.time <= 0) == event0
-    assert env.next_event_by_condition(lambda env, event: event.time == 0) == event1
-    assert env.next_event_by_condition(lambda env, event: event.time >= 0) == event1
-    assert env.next_event_by_condition(lambda env, event: event.time > 0) is event2
+    assert env.peek_by_condition(lambda env, event: event.time < 0) is -1
+    assert env.peek_by_condition(lambda env, event: event.time <= 0) == -1
+    assert env.peek_by_condition(lambda env, event: event.time == 0) == 0
+    assert env.peek_by_condition(lambda env, event: event.time >= 0) == 0
+    assert env.peek_by_condition(lambda env, event: event.time > 0) is 1
 
     # NOTE: Cases related to t=1
-    assert env.next_event_by_condition(lambda env, event: event.time < 1) is event0
-    assert env.next_event_by_condition(lambda env, event: event.time <= 1) == event0
-    assert env.next_event_by_condition(lambda env, event: event.time == 1) == event2
-    assert env.next_event_by_condition(lambda env, event: event.time >= 1) == event2
-    assert env.next_event_by_condition(lambda env, event: event.time > 1) is None
+    assert env.peek_by_condition(lambda env, event: event.time < 1) is -1
+    assert env.peek_by_condition(lambda env, event: event.time <= 1) == -1
+    assert env.peek_by_condition(lambda env, event: event.time == 1) == 1
+    assert env.peek_by_condition(lambda env, event: event.time >= 1) == 1
+    assert env.peek_by_condition(lambda env, event: event.time > 1) is None
 
 
 def test_next_by_if_action():
@@ -49,19 +49,20 @@ def test_next_by_if_action():
     assert condition0(env, event0) == True
     assert condition0(env, event1) == True
     assert condition0(env, event2) == False
-    assert env.next_event_by_condition(condition0) == event0
+    assert env.peek_by_condition(condition0) == 0
 
     condition1 = lambda env, event: event.action() == 2018
     assert condition1(env, event0) == False
     assert condition1(env, event1) == False
     assert condition1(env, event2) == True
-    assert env.next_event_by_condition(condition1) == event2
+    assert env.peek_by_condition(condition1) == 0
 
     condition2 = lambda env, event: event.action == 2018
     assert condition2(env, event0) == False
     assert condition2(env, event1) == False
     assert condition2(env, event2) == False
-    assert env.next_event_by_condition(condition2) is None
+    assert env.peek_by_condition(condition2) is None
 
 
 # TODO: Add more test cases...
+
